2348e411238b464420f28bbd75b351407f5c7ef4
BradySmith6357 <bsmit6357@gmail.com>
2016-02-29 16:09:05 -0700

    first commit

---
+++ pset.js
@@ -0,0 +1,9 @@
+var addNumbers = function(string){
+	for(i=0; i <= string.length; i++){
+		if (isNaN(string.length[i]) = true){
+			string.replace(string.length[i],"")
+		}
+	}
+}
+
+console.log(addNumbers("88Hello 3World!"))
